584e143bb09fff1474b02e2412d668d11a11dd44
Tommaso Bruno <bruno.tommaso@protonmail.com>
2023-12-21 15:18:10 +0000

    feat: add loop and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,26 @@ fn main() {
     let random = rand::thread_rng().gen_range(1..100);
 
     let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read message");
 
-    println!("Secret number is {random}");
-    let input: u32 = input.trim().parse().expect("Type a number");
+    loop {
+        println!("Input your number");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read message");
 
-    match input.cmp(&random) {
-        Ordering::Less => println!("Too low!"),
-        Ordering::Equal => println!("Your guess is correct!"),
-        Ordering::Greater => println!("Too high!"),
+        println!("Secret number is {random}");
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match input.cmp(&random) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Equal => {
+                println!("Your guess is correct!");
+                break;
+            }
+            Ordering::Greater => println!("Too high!"),
+        }
     }
 }
